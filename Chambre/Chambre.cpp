253789eebdb01b31c4ae84570e4c7eca3e546c81
PeterBachour <peter_bachour@icloud.com>
2019-10-24 11:24:21 +0200

    [ADD] add Chambre

---
+++ Chambre/Chambre.cpp
@@ -0,0 +1,38 @@
+#include "Date.h"
+#include <string>
+
+//constructors//
+Date::Date(int day, int month, int year) : m_day(day), m_month(month), m_year(year)
+{}
+
+//getters//
+int Date::getDay()
+{
+  return m_day;
+}
+
+int Date::getMonth()
+{
+  return m_month;
+}
+
+int Date::getYear()
+{
+  return m_year;
+}
+
+//setters//
+void Date::setDay(int day)
+{
+  m_day = day;
+}
+
+void Date::setMonth(int month)
+{
+  m_month = month;
+}
+
+void Date::setYear(int year)
+{
+  m_year = year;
+}
